8fb2335f87e55a9b6fb9f6e4b97978bf3f89b378
Jonathanvanhaaften <Jonathanvanhaaften@gmail.com>
2021-04-17 04:05:13 -0600

    starting to add text

--- admin/frontend/src/components/Main/Dashboard.js
+++ admin/frontend/src/components/Main/Dashboard.js
@@ -5,6 +5,10 @@ export default function Dashboard() {
     return (
         <Fragment>
             <img className="h-auto, w-screen"src={hero} alt="skyline"></img>
+            <div>
+                <h1>You Adventures are here</h1>
+                <p>let us take you to the moutians throw the valleys and peaks of the great devide. Follow our guides into the wilds of Canada's peaks</p>
+            </div>
         </Fragment>
 
     )
